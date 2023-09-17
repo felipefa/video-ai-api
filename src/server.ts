c6a58ee5a0b0ed3fc2b6f2c2519f037eb7a91bf1
Felipe Araujo <devaraujofelipe@gmail.com>
2023-09-17 18:43:27 +0200

    chore: create node project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { fastify } from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return "Hello world";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP Server is running on port 3333")
+});
