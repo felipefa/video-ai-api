44d0be7cb3aed5915508627ad3fac1820c3fa3dd
Felipe Araujo <devaraujofelipe@gmail.com>
2023-09-21 15:48:05 +0200

    feat: add createTranscriptionRoute

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from "fastify";
 
+import { createTranscriptionRoute } from "./routes/create-transcription";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
+app.register(createTranscriptionRoute);
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 
