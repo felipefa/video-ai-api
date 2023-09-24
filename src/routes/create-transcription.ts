1ea995b53e24c33f26b1817e3cd9564f3da2f529
Felipe Araujo <devaraujofelipe@gmail.com>
2023-09-24 21:02:06 +0200

    chore: configure cors

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -32,7 +32,7 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     const response = await openai.audio.transcriptions.create({
       file: audioReadStream,
       model: "whisper-1",
-      language: "pt",
+      language: "en",
       response_format: "json",
       temperature: 0,
       prompt,
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+import "dotenv/config";
+
+import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 
 import { createTranscriptionRoute } from "./routes/create-transcription";
@@ -7,11 +10,15 @@ import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
+app.register(fastifyCors, {
+  origin: process.env.FRONTEND_URL,
+});
+
 app.register(createTranscriptionRoute);
 app.register(generateAICompletionRoute);
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP Server is running on port 3333")
+  console.log("HTTP Server is running on port 3333");
 });
